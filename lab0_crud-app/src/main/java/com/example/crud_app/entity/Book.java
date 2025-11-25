a5114675d2ced22583d12491dddc80300a44df63
Pechenka1234 <153770969+Pechenka1234@users.noreply.github.com>
2025-11-25 23:14:44 +0200

    first commit

---
+++ lab0_crud-app/src/main/java/com/example/crud_app/entity/Book.java
@@ -0,0 +1,16 @@
+package com.example.crudapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "books")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+}
